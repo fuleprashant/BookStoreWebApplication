30d4b3d3fa65b85f5a4f7ca2f577a64a310777d7
Prashant Fule <tbs.prasanth@gmail.com>
2024-11-07 18:32:42 +0530

    add the corses data in the project and solve overloaping isue

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,17 +4,19 @@ const Card = ({ item }) => {
   console.log("the item", item);
   return (
     <div>
-      <div class="relative mx-[20px] md:mx-[100px] flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96 ">
+      <div class="relative mx-[20px] md:mx-[100px] flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg  w-80 md:w-96 ">
         <div class="relative h-56 m-2.5 overflow-hidden text-white rounded-md">
-          <img src={item.image} alt="card-image" />
+          <img src={item?.image} alt="card-image" />
         </div>
         <div class="p-4">
-          <h6 class="mb-2 text-slate-800 text-xl font-semibold">{item.name}</h6>
-          <p class="text-slate-600 leading-normal font-light">{item.title}</p>
+          <h6 class="mb-2 text-slate-800 text-xl font-semibold">
+            {item?.name}
+          </h6>
+          <p class="text-slate-600 leading-normal font-light">{item?.title}</p>
           <p class="text-slate-600 leading-normal font-light bg-pink-50">
-            {item.category}
+            {item?.category}
           </p>
-          <p class="text-slate-600 leading-normal font-light">{item.price}</p>
+          <p class="text-slate-600 leading-normal font-light">{item?.price}</p>
         </div>
         <div class="px-4 pb-4 pt-0 mt-2">
           <button
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { BiSearch } from "react-icons/bi";
 import { CiLight } from "react-icons/ci";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { MdDarkMode } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [themeMode, setThemeMode] = useState(false);
@@ -31,7 +32,92 @@ const Navbar = () => {
   const toggleMobileView = () => {
     setMobilePopup(!mobilePopup);
   };
+  // return (
+  //   <div>
+  //     <div
+  //       className={`h-16 bg-slate-200 flex items-center ${
+  //         sticky ? "fixed top-0 w-full z-10 shadow-md" : ""
+  //       } transition-all duration-300`}
+  //     >
+  //       <div className="md:hidden flex items-center ml-3">
+  //         <button onClick={toggleMobileView}>
+  //           {mobilePopup ? <FaTimes size={24} /> : <FaBars size={24} />}
+  //         </button>
+  //       </div>
+  //       <div className="mx-[60px] md:mx-[100px] w-full flex items-center justify-between z-10">
+  //         {/* Header title */}
+  //         <div className="text-xl font-bold text-center whitespace-nowrap">
+  //           Swarajya BookStore
+  //         </div>
 
+  //         <div className="flex gap-5 items-center">
+  //           <ul className="hidden md:flex gap-8">
+  //             <li>
+  //               <NavLink to="/">Home</NavLink>
+  //             </li>
+  //             <li>
+  //               <NavLink to="/courses">Courses</NavLink>
+  //             </li>
+  //             <li>
+  //               <NavLink to="">Contact</NavLink>
+  //             </li>
+  //             <li>
+  //               <NavLink to="">About</NavLink>
+  //             </li>
+  //           </ul>
+
+  //           {/* Mobile dropdown menu */}
+  //           <div
+  //             className={`${
+  //               mobilePopup ? "block" : "hidden"
+  //             } absolute top-16 left-0 w-full bg-slate-200 md:hidden shadow-md flex flex-col items-center gap-16 py-4`}
+  //           >
+  //             <ul>
+  //               <li>
+  //                 <a className="block text-center py-3">Home</a>
+  //               </li>
+  //               <li>
+  //                 <NavLink className="block text-center py-3">Courses</NavLink>
+  //               </li>
+  //               <li>
+  //                 <a className="block text-center py-3">Contact</a>
+  //               </li>
+  //               <li>
+  //                 <a className="block text-center py-3">About</a>
+  //               </li>
+  //             </ul>
+  //           </div>
+
+  //           {/* Search input */}
+  //           <div className="hidden md:block relative w-full max-w-xs">
+  //             <input
+  //               type="text"
+  //               placeholder="Search..."
+  //               className="w-full p-3 pl-10 pr-4 border border-gray-300 rounded-md text-gray-700 placeholder-gray-400 shadow-md focus:outline-none focus:ring-2 focus:ring-slate-200"
+  //             />
+  //             <BiSearch
+  //               className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+  //               size={20}
+  //             />
+  //           </div>
+
+  //           {/* Theme toggle */}
+  //           <div
+  //             onClick={toggleTheme}
+  //             className="cursor-pointer text-gray-800 dark:text-white"
+  //           >
+  //             {themeMode ? <CiLight size={24} /> : <MdDarkMode size={24} />}
+  //           </div>
+
+  //           {/* Login button */}
+  //           <button className="px-3 py-2 bg-black text-white font-semibold rounded-md shadow-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-black transition duration-300">
+  //             Login
+  //           </button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
   return (
     <div>
       {/* <div
@@ -41,7 +127,7 @@ const Navbar = () => {
     ></div> */}
       <div
         className={`h-16 bg-slate-200 flex items-center ${
-          sticky ? "fixed w-full " : ""
+          sticky ? "fixed w-full z-10 top-0 shadow-md " : ""
         }`}
       >
         <div className="md:hidden flex items-center ml-3">
@@ -49,7 +135,7 @@ const Navbar = () => {
             {mobilePopup ? <FaTimes size={24} /> : <FaBars size={24} />}
           </button>
         </div>
-        <div className="mx-[60px] md:mx-[100px] w-full flex items-center justify-between ">
+        <div className="mx-[60px] md:mx-[100px] w-full flex items-center justify-between z-10">
           {/* when text comein two line instead of one line we use below class = whiteapce-nowrap */}
           <div className="text-xl font-bold  text-center  whitespace-nowrap ">
             Swarajya BookStore
@@ -60,16 +146,16 @@ const Navbar = () => {
           <div className="flex gap-5 items-center">
             <ul className="hidden md:flex  gap-8">
               <li>
-                <a href="">Home</a>
+                <NavLink to="/">Home</NavLink>
               </li>{" "}
               <li>
-                <a href="">Courses</a>
+                <NavLink to="/courses">Courses</NavLink>
               </li>{" "}
               <li>
-                <a href="">Contact</a>
+                <NavLink to="">Contact</NavLink>
               </li>{" "}
               <li>
-                <a href="">About</a>
+                <NavLink to="">About</NavLink>
               </li>
             </ul>
             <div
@@ -82,7 +168,7 @@ const Navbar = () => {
                   <a className="block text-center py-3">Home</a>
                 </li>
                 <li>
-                  <a className="block text-center py-3">Courses</a>
+                  <NavLink className="block text-center py-3">Courses</NavLink>
                 </li>
                 <li>
                   <a className="block text-center py-3">Contact</a>
