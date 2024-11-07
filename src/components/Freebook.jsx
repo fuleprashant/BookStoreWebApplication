b6bffa49343da3091f706284bfec24a7519761b6
Prashant Fule <tbs.prasanth@gmail.com>
2024-11-07 17:06:10 +0530

    we add the card and the slider library of react

--- src/components/Freebook.jsx
+++ src/components/Freebook.jsx
@@ -3,6 +3,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import list from "../../public/list.json";
+import Card from "./Card";
 
 const Freebook = () => {
   const filterData = list.filter((data) => data.category === "Free");
@@ -60,24 +61,9 @@ const Freebook = () => {
       </div>
       <div>
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
+          {filterData.map((item) => (
+            <Card item={item} key={item.id} />
+          ))}
         </Slider>
       </div>
     </>
