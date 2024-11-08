493112ef00f4490f0dde13cd9bf9b63e918a2ef9
Prashant Fule <tbs.prasanth@gmail.com>
2024-11-08 15:00:20 +0530

    connected database to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import db from "./database/db.js";
 
 const app = express();
 dotenv.config();
 // console.log(app);
 
 const port = process.env.PORT;
+
+db();
+// app.get("/", (req, res) => {
+//   res.send("the server has been start o");
+// });
 
 app.listen(port, () => {
   console.log(`the server run on the port number http://localhost:${port}`);
