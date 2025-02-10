040cab49e31243ddd8b4d3a0d7fe6613c0c625b2
Prashant Fule <bhavik.shah@lancerworldwide.com>
2025-02-10 14:44:26 +0530

    add the data for the production

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ import db from "./database/db.js";
 import cors from "cors";
 import userRouter from "./routes/user.route.js"; // Import your user router
 import bookRouter from "./routes/book.route.js";
+import path from "path";
 const app = express();
+const _dirname = path.resolve();
 app.use(cors());
 
 app.use(express.json());
@@ -19,6 +21,11 @@ db();
 // });
 app.use("/book", bookRouter);
 app.use("/user", userRouter);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 app.listen(port, () => {
   console.log(`the server run on the port number http://localhost:${port}`);
 });
